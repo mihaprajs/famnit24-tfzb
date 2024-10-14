773c36e6907ad7c0851f1adc6c555b6f90f26814
Miha Prajs <108951795+mihaprajs@users.noreply.github.com>
2024-10-14 23:33:11 +0200

    DN2

---
+++ HW/HW2/HW2.m
@@ -0,0 +1,43 @@
+% HW2
+
+%% task a)
+% Because η is defined as velocity of the river over velocity of the boat it means that there are three cases.
+% Case #1: η < 1: This means that the boat is faster than the river. The boat needs to steer slightly upstream to compensate for slower river.
+% Case #2: η = 1: This means that the boat is as fast as the river. The boat will need to steer at an angel that is not to extreme.
+% Case #3: η > 1: This means that the boat is slower than the river. The boat needs to steer at mostly upstream, because the river is puhing it downstream.
+
+%% task b)
+syms alpha
+eta_value = [1.5,2.0,2.5,3.0];
+L = 1; % river width
+for eta = eta_value
+    d_alpha = L * cos(alpha) / sin(alpha) + L * eta / sin(alpha); % define function (L could be skipped as it is set to 1)
+    d_alpha_derivative = diff(d_alpha, alpha); % get the derivative of the function
+    optimal_alpha = solve(d_alpha_derivative == 0, alpha); 
+    optimal_value = eval(optimal_alpha); % get numeric value - because it is an array of two numbers I'm using only the one on the 1st index
+    fprintf('Optimal angle for η = %.1f: %.2f°\n', eta, rad2deg(optimal_value(1))); % using rad2deg to get the number in degrees
+end
+
+%% task c)
+f = @(alpha, eta) L * cosd(alpha) ./ sind(alpha) + L * eta ./ sind(alpha); % defining function to be used for ploting
+alpha_values = linspace(0.01, pi-0.01, 100); % avoid division by 0
+
+% graph
+figure;
+hold on;
+plot(rad2deg(alpha_values), f(alpha_values, 1.5), 'r', 'DisplayName', 'η = 1.5');
+plot(rad2deg(alpha_values), f(alpha_values, 2.0), 'g', 'DisplayName', 'η = 2.0');
+plot(rad2deg(alpha_values), f(alpha_values, 2.5), 'b', 'DisplayName', 'η = 2.5');
+plot(rad2deg(alpha_values), f(alpha_values, 3.0), 'm', 'DisplayName', 'η = 3.0');
+xlabel('Angle (degrees)');
+ylabel('Distance d(α)');
+title('Distance vs Angle for Different η Values');
+hold off;
+
+%% task d)
+eta_val = 2; % value assigned in pdf
+alpha_val = 120; % value assigned in pdf
+
+d_val = L * cosd(alpha_val) / sind(alpha_val) + L * eta_val / sind(alpha_val); % Calculate the distance d(α)
+
+disp(['Distance for η = 2 and α = 120°: ', num2str(d_val)]);
